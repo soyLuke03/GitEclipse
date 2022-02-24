2a1b9616c9a0ff3f53f7c4545a2dc773fc920243
unknown <luquelectronico13@gmail.com>
2022-02-24 12:27:36 +0100

    jo

---
+++ src/paqueto/Clasesita.java
@@ -0,0 +1,10 @@
+package paqueto;
+
+public class Clasesita {
+
+	public static void main(String[] args) {
+		System.out.println("hola");
+		System.out.println("cambio");
+		System.out.println("OTro cambio");
+	}
+}
